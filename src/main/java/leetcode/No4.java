7533e71019a6f71413b3b9386c933272049eed16
zhaochao <zhaochao>
2021-02-08 17:40:44 +0800

    No4

--- src/main/java/leetcode/No4.java
+++ src/main/java/leetcode/No4.java
@@ -44,8 +44,8 @@ package leetcode;
  */
 public class No4 {
     public static void main(String[] args) {
-        int[] a = {};
-        int[] b = {2, 3};
+        int[] a = {1, 2};
+        int[] b = {-1, 3};
         No4 no4 = new No4();
         Double c = no4.findMedianSortedArrays(a, b);
         System.out.print(c);
@@ -53,79 +53,6 @@ public class No4 {
 
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int len1 = nums1.length;
-        int len2 = nums2.length;
-        int index1 = 0;
-        int index2 = 0;
-        //总数为偶
-        if ((len1 + len2) % 2 == 0) {
-            while (index1 < len1 && index2 < len2 && (index1 + index2) < (len1 + len2 - 2) / 2) {
-                if (nums1[index1] <= nums2[index2]) {
-                    index1++;
-                } else {
-                    index2++;
-                }
-            }
-            //如果总步数已经凑齐
-            if (((index1 + index2) == (len1 + len2 - 2) / 2)) {
-                if (len1 == 0) {
-                    return (nums2[index2] + nums2[index2 + 1]) / 2.0;
-                } else if (len2 == 0) {
-                    return (nums1[index1] + nums1[index1 + 1]) / 2.0;
-
-                } else {
-                    return (nums1[index1] + nums2[index2]) / 2.0;
-                }
-            }
-            //有一个走完了
-            else {
-                //哪个到了，nums1到头了
-                if (index1 == len1) {
-
-                    return (nums2[(len1 + len2 - 2) / 2 - len1] + nums2[(len1 + len2 - 2) / 2 - len1 + 1]) / 2;
-                } else {
-                    return (nums1[(len1 + len2 - 2) / 2 - len2] + nums1[(len1 + len2 - 2) / 2 - len2 + 1]) / 2;
-                }
-
-            }
-
-
-        }
-        //总数为奇
-        else {
-            //如果总步数已经凑齐
-            while (index1 < len1 && index2 < len2 && (index1 + index2) < (len1 + len2 - 2) / 2 + 1) {
-                if (nums1[index1] <= nums2[index2]) {
-                    index1++;
-                } else {
-                    index2++;
-                }
-            }
-
-
-            //如果两个都没做完
-            if (((index1 + index2) == (len1 + len2 - 1) / 2)) {
-                if (len1 == 0) {
-                    return nums2[index2];
-                } else if (len2 == 0) {
-                    return nums1[index1];
-                } else {
-                    return nums1[index1] > nums2[index2] ? nums2[index2] : nums1[index1];
-                }
-
-            }
-
-            //有一个走完了
-            else {
-                //哪个到了，nums1到头了
-                if (index1 == len1) {
-                    return nums2[(len1 + len2 - 2 + 1) / 2 - len1];
-                } else {
-                    return nums1[(len1 + len2 - 2 + 1) / 2 - len2];
-                }
-
-            }
-        }
 
     }
 }
