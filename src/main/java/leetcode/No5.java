4be9bd70917ef104fec7e385338ed461499ee407
zhaochao <zhaochao>
2021-03-09 13:57:39 +0800

    jvm第三版

---
+++ src/main/java/leetcode/No5.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+/**
+ * @Author: zhaochao
+ * @Date: 2021/3/8 14:29
+ */
+
+/**
+ * 5. 最长回文子串
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "babad"
+ * 输出："bab"
+ * 解释："aba" 同样是符合题意的答案。
+ * 示例 2：
+ * <p>
+ * 输入：s = "cbbd"
+ * 输出："bb"
+ * 示例 3：
+ * <p>
+ * 输入：s = "a"
+ * 输出："a"
+ * 示例 4：
+ * <p>
+ * 输入：s = "ac"
+ * 输出："a"
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 1000
+ * s 仅由数字和英文字母（大写和/或小写）组成
+ */
+public class No5 {
+
+
+    public String longestPalindrome(String s) {
+
+        return "";
+    }
+
+
+}
