8439de803305767fbf4ab1d0510668fa6ac852ef
zhaochao <zhaochao>
2021-03-10 15:03:19 +0800

    提交

---
+++ src/main/java/code/QRCodeImage.java
@@ -0,0 +1,45 @@
+package code;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+/**
+ * @Author: zhaochao
+ * @Date: 2021/3/10 14:46
+ */
+public class QRCodeImage {
+
+    private static final String QR_CODE_IMAGE_PATH = "F:/MyQRCode.png";
+
+    private static void generateQRCodeImage(String text, int width, int height, String filePath) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        Path path = FileSystems.getDefault().getPath(filePath);
+
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            String contents = "小达子，你干嘛呢";
+            contents = new String(contents.getBytes("UTF-8"), "ISO-8859-1");//如果不想更改源码，则将字符串转换成ISO-8859-1编码
+            generateQRCodeImage(contents, 350, 350, QR_CODE_IMAGE_PATH);
+        } catch (WriterException e) {
+            System.out.println("Could not generate QR Code, WriterException :: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Could not generate QR Code, IOException :: " + e.getMessage());
+        }
+
+    }
+}
