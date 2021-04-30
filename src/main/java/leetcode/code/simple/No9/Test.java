7e83e7debc9ec1a0c42bbf567402679d8d28fa68
zhaochao <zhaochao6@xdf.cn>
2021-04-30 18:31:42 +0800

    经纬度转换

--- src/main/java/leetcode/code/simple/No9/Test.java
+++ src/main/java/leetcode/code/simple/No9/Test.java
@@ -5,4 +5,17 @@ package leetcode.code.simple.No9;
  * @date 2021/4/30 18:22
  */
 public class Test {
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        if (x < 10) {
+            return true;
+        }
+        while (x >= 0) {
+        }
+
+
+    }
 }
