d81894f6c12a39f81ea56ab1c4b51c9966442b55
zhaochao <zhaochao6@xdf.cn>
2021-05-06 20:21:54 +0800

    经纬度转换

---
+++ src/main/java/leetcode/code/simple/No13/Test.java
@@ -0,0 +1,8 @@
+package leetcode.code.simple.No13;
+
+/**
+ * @author zhaochao
+ * @date 2021/5/6 20:16
+ */
+public class Test {
+}
--- src/main/java/leetcode/code/simple/No9/Test.java
+++ src/main/java/leetcode/code/simple/No9/Test.java
@@ -6,6 +6,12 @@ package leetcode.code.simple.No9;
  */
 public class Test {
 
+    public static void main(String[] args) {
+        Test test = new Test();
+        boolean data = test.isPalindrome(454454);
+        System.out.println();
+    }
+
     public boolean isPalindrome(int x) {
         if (x < 0) {
             return false;
@@ -13,9 +19,17 @@ public class Test {
         if (x < 10) {
             return true;
         }
-        while (x >= 0) {
+        char[] chars = String.valueOf(x).toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+        while (start <= end) {
+            if (chars[start] != chars[end]) {
+                return false;
+            }
+            start++;
+            end--;
         }
 
-
+        return true;
     }
 }
