ab7e155bbd0963eaf45546adaec39cae9c2648b6
zhaochao <zhaochao6@xdf.cn>
2021-05-10 17:57:34 +0800

    No21

---
+++ src/main/java/leetcode/code/simple/No21/ListNode.java
@@ -0,0 +1,22 @@
+package leetcode.code.simple.No21;
+
+/**
+ * @author zhaochao
+ * @date 2021/5/10 17:56
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ src/main/java/leetcode/code/simple/No21/Test.java
@@ -0,0 +1,15 @@
+package leetcode.code.simple.No21;
+
+/**
+ * @author zhaochao
+ * @date 2021/5/10 17:51
+ */
+public class Test {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+
+        return null;
+
+    }
+}
