b67ab78553a4796481ff911e6ffca24898d00498
zhaochao <zhaochao6@xdf.cn>
2021-05-26 21:12:26 +0800

    经纬度转换

---
+++ src/main/java/leetcode/code/simple/No226没想法/Example.java
@@ -0,0 +1,8 @@
+package leetcode.code.simple.No226没想法;
+
+/**
+ * @author zhaochao
+ * @date 2021/5/26 20:54
+ */
+public class Example {
+}
---
+++ src/main/java/leetcode/code/simple/No226没想法/TreeNode.java
@@ -0,0 +1,21 @@
+package leetcode.code.simple.No226没想法;
+
+/**
+ * @author zhaochao
+ * @date 2021/5/26 20:55
+ */
+public class TreeNode {
+
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+}
